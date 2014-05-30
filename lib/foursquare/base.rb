6d8e3c170869602bbcc2545368c2a4f482083001
Benjamin Shyong <ben.shyong@gmail.com>
2014-05-30 18:07:09 -0400

    version update

--- lib/foursquare/base.rb
+++ lib/foursquare/base.rb
@@ -8,7 +8,7 @@ module Foursquare
     # added version to make sure we are using the correct API version
     # see https://groups.google.com/forum/#!topic/foursquare-api/OGLePZU8VXQ
     # https://developer.foursquare.com/docs/overview.html#versioning
-    VERSION = "20111129"
+    VERSION = "20140530"
 
     def initialize(*args)
       case args.size
